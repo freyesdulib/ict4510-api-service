8301002d32bd7ac055767e291a9428654fb23d5b
Fernando <fernando.reyes@du.edu>
2019-04-08 21:55:47 -0600

    added PUT request in allowed access control methods config

--- config/express.js
+++ config/express.js
@@ -21,7 +21,7 @@ module.exports = function () {
 
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'DELETE');
+        res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
         res.header('Access-Control-Allow-Headers', '*');
         next();
     });
