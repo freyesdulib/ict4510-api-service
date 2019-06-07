7a51835bac7461b9cb0d8ff63043d511b5c74959
Fernando <fernando.reyes@du.edu>
2019-06-07 10:55:36 -0600

    changed allow header value to wildcard

--- config/express.js
+++ config/express.js
@@ -22,7 +22,7 @@ module.exports = function () {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'OPTIONS', 'DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type', 'x-access-token');
+        res.header('Access-Control-Allow-Headers', '*');
         next();
     });
 
